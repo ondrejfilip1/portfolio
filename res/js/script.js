b289dfe8b15bad170647db16dbb57c499c78430b
ondrejfilip1 <ondrej.filip@student.spsmb.cz>
2023-11-19 00:40:42 +0100

    Added light/dark mode switch

--- res/js/script.js
+++ res/js/script.js
@@ -37,3 +37,21 @@ document.addEventListener('mouseleave', () => {
 document.addEventListener('mouseenter', () => {
     cursor.style.opacity = '1';
 });
+
+const checkbox = document.getElementById("dark-light-mode");
+const images = document.querySelectorAll(".image");
+checkbox.addEventListener("change", () => {
+    if (document.documentElement.style.filter === "invert(1)") {
+        document.documentElement.style.filter = "";
+        images.forEach((element) => {
+            element.style.filter = "";
+        });
+    } else {
+        document.documentElement.style.filter = "invert(1)";
+        images.forEach((element) => {
+            element.style.filter = "invert(1)";
+        });
+    }
+})
+
+/* mix-blend-mode: difference*/
