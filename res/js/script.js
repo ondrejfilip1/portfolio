484d40b60fccecbab277cd8f4d6e7974da8b353d
ondrejfilip1 <ondrej.filip@student.spsmb.cz>
2023-11-23 21:16:58 +0100

    stuff

--- res/js/script.js
+++ res/js/script.js
@@ -56,4 +56,47 @@ checkbox.addEventListener("change", () => {
   }
 });
 
-/* mix-blend-mode: difference*/
+/* https://webdesign.tutsplus.com/animate-on-scroll-with-javascript--cms-36671t */
+/* Thanks for the scrolling effect :3 */
+
+const scrollElements = document.querySelectorAll(".onScroll");
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.25)) {
+      displayScrollElement(el);
+    } else {
+      hideScrollElement(el);
+    }
+  });
+};
+
+let isScrollingUp = false;
+let lastScrollTop = 0;
+
+window.addEventListener("scroll", () => {
+  const currentScroll =
+    window.pageYOffset || document.documentElement.scrollTop;
+
+  isScrollingUp = currentScroll < lastScrollTop;
+
+  handleScrollAnimation();
+
+  lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+});
