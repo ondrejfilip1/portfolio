60fede3b2ca20dbc86ac1172ceca100abde02d68
ondrejfilip1 <ondrej.filip@student.spsmb.cz>
2023-11-23 20:07:55 +0100

    Added light/dark switch icons

--- res/js/script.js
+++ res/js/script.js
@@ -1,57 +1,59 @@
-const cursor = document.querySelector('.js-cursor');
+const cursor = document.querySelector(".js-cursor");
 
 function moveCursor(e) {
-    cursor.style.transform = `translate(${e.clientX - 10}px, ${e.clientY - 10}px)`;
+  cursor.style.transform = `translate(${e.clientX - 10}px, ${
+    e.clientY - 10
+  }px)`;
 }
 
-document.addEventListener('mousemove', moveCursor);
+document.addEventListener("mousemove", moveCursor);
 
-const biggerCursor = document.querySelectorAll('.bigger-cursor');
+const biggerCursor = document.querySelectorAll(".bigger-cursor");
 
 biggerCursor.forEach((element) => {
-    element.addEventListener('mouseover', () => {
-        cursor.classList.add('hovered');
-    });
+  element.addEventListener("mouseover", () => {
+    cursor.classList.add("hovered");
+  });
 
-    element.addEventListener('mouseout', () => {
-        cursor.classList.remove('hovered');
-    });
+  element.addEventListener("mouseout", () => {
+    cursor.classList.remove("hovered");
+  });
 });
 
-const linkCursor = document.querySelectorAll('.link-cursor');
+const linkCursor = document.querySelectorAll(".link-cursor");
 
 linkCursor.forEach((element) => {
-    element.addEventListener('mouseover', () => {
-        cursor.classList.add('hovered-link');
-    });
+  element.addEventListener("mouseover", () => {
+    cursor.classList.add("hovered-link");
+  });
 
-    element.addEventListener('mouseout', () => {
-        cursor.classList.remove('hovered-link');
-    });
+  element.addEventListener("mouseout", () => {
+    cursor.classList.remove("hovered-link");
+  });
 });
 
-document.addEventListener('mouseleave', () => {
-    cursor.style.opacity = '0';
+document.addEventListener("mouseleave", () => {
+  cursor.style.opacity = "0";
 });
 
-document.addEventListener('mouseenter', () => {
-    cursor.style.opacity = '1';
+document.addEventListener("mouseenter", () => {
+  cursor.style.opacity = "1";
 });
 
 const checkbox = document.getElementById("dark-light-mode");
 const images = document.querySelectorAll(".image");
 checkbox.addEventListener("change", () => {
-    if (document.documentElement.style.filter === "invert(1)") {
-        document.documentElement.style.filter = "";
-        images.forEach((element) => {
-            element.style.filter = "";
-        });
-    } else {
-        document.documentElement.style.filter = "invert(1)";
-        images.forEach((element) => {
-            element.style.filter = "invert(1)";
-        });
-    }
-})
+  if (document.documentElement.style.filter === "invert(1)") {
+    document.documentElement.style.filter = "";
+    images.forEach((element) => {
+      element.style.filter = "";
+    });
+  } else {
+    document.documentElement.style.filter = "invert(1)";
+    images.forEach((element) => {
+      element.style.filter = "invert(1)";
+    });
+  }
+});
 
 /* mix-blend-mode: difference*/
