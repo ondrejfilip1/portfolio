0f9f6de2d4aafec99d5e0458852b96142011a424
Ondřej Filip <144805772+ondrejfilip1@users.noreply.github.com>
2024-02-21 17:00:32 +0100

    Update script.js

--- res/js/script.js
+++ res/js/script.js
@@ -1,12 +1,19 @@
 const cursor = document.querySelector(".js-cursor");
+let mouseX = 0;
+let mouseY = 0;
 
-function moveCursor(e) {
-  cursor.style.transform = `translate(${e.clientX - 10}px, ${
-    e.clientY - 10
-  }px)`;
+const moveCursor = (e) => {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+  requestAnimationFrame(updateCursor);
 }
 
-document.addEventListener("mousemove", moveCursor);
+const updateCursor = () => {
+  cursor.style.top = `${mouseY - 10}px`;
+  cursor.style.left = `${mouseX - 10}px`;
+  descriptionWork.style.left = `${mouseX}px`;
+  descriptionWork.style.top = `${mouseY}px`;
+}
 
 const biggerCursor = document.querySelectorAll(".bigger-cursor");
 
@@ -40,20 +47,17 @@ document.addEventListener("mouseenter", () => {
   cursor.style.opacity = "1";
 });
 
+
 const checkbox = document.getElementById("dark-light-mode");
 const images = document.querySelectorAll(".image");
+
 checkbox.addEventListener("change", () => {
-  if (document.documentElement.style.filter === "invert(1)") {
-    document.documentElement.style.filter = "";
-    images.forEach((element) => {
-      element.style.filter = "";
-    });
-  } else {
-    document.documentElement.style.filter = "invert(1)";
-    images.forEach((element) => {
-      element.style.filter = "invert(1)";
-    });
-  }
+  const invertFilter = document.documentElement.style.filter === "invert(1)";
+  document.documentElement.style.filter = invertFilter ? "" : "invert(1)";
+  // Invert colors of images
+  images.forEach((element) => {
+    element.style.filter = invertFilter ? "" : "invert(1)";
+  });
 });
 
 /* https://webdesign.tutsplus.com/animate-on-scroll-with-javascript--cms-36671t */
@@ -99,4 +103,50 @@ window.addEventListener("scroll", () => {
   handleScrollAnimation();
 
   lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+});
+
+const descriptionWork = document.querySelector('.description-work');
+const workClass = document.querySelectorAll('.work');
+
+const updateDescription = (e) => {
+  let target = e.target;
+  // find ID that starts with
+  while (target && !target.id.startsWith('work-hover-')) {
+    target = target.parentElement;
+  }
+
+  if (target && target.id.startsWith('work-hover-')) {
+    switch (target.id) {
+      case 'work-hover-1':
+        descriptionWork.innerHTML = 'Paintings by Tim Lee<br><span class="yellow-text">Bootstrap, Custom CSS</span>';
+        break;
+      case 'work-hover-2':
+        descriptionWork.innerHTML = 'Cookie clicker<br><span class="yellow-text">Custom CSS & JS</span>';
+        break;
+      case 'work-hover-3':
+        descriptionWork.innerHTML = 'Modern school website<br><span class="yellow-text" style="">Bootstrap, Custom CSS</span>';
+        break;
+      default:
+        descriptionWork.innerText = "";
+    }
+  }
+}
+
+document.addEventListener("mousemove", moveCursor);
+
+const startFollowing = () => {
+  document.addEventListener('mousemove', updateDescription);
+  descriptionWork.style.opacity = "1";
+  descriptionWork.style.scale = "1";
+}
+
+const stopFollowing = () =>  {
+  document.removeEventListener('mousemove', updateDescription);
+  descriptionWork.style.opacity = "0";
+  descriptionWork.style.scale = "0.8";
+}
+
+workClass.forEach(function(workClasses) {
+  workClasses.addEventListener('mouseover', startFollowing);
+  workClasses.addEventListener('mouseout', stopFollowing);
 });
