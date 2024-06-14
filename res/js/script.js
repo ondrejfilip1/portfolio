0328dc8cf10f2dc77ef923d394b8952a6adb8349
Ondřej Filip <144805772+ondrejfilip1@users.noreply.github.com>
2024-06-14 20:37:11 +0200

    Fixed box shadow on work description

--- res/js/script.js
+++ res/js/script.js
@@ -9,35 +9,40 @@ document.body.onload = () => {
   const randomNumber = Math.floor(Math.random() * 4);
   console.log(randomNumber);
   if (randomNumber == 0) {
-    gradientText.style.background = "-webkit-linear-gradient(0deg, #642b73, #c6426e, #642b73, #c6426e)";
+    gradientText.style.background =
+      "-webkit-linear-gradient(0deg, #642b73, #c6426e, #642b73, #c6426e)";
   } else if (randomNumber == 1) {
-    gradientText.style.background = "-webkit-linear-gradient(0deg, #4568dc, #b06ab3, #4568dc, #b06ab3)";
+    gradientText.style.background =
+      "-webkit-linear-gradient(0deg, #4568dc, #b06ab3, #4568dc, #b06ab3)";
   } else if (randomNumber == 2) {
-    gradientText.style.background = "-webkit-linear-gradient(0deg, #ff5f6d, #ffc371, #ff5f6d, #ffc371)";
+    gradientText.style.background =
+      "-webkit-linear-gradient(0deg, #ff5f6d, #ffc371, #ff5f6d, #ffc371)";
   } else if (randomNumber == 3) {
-    gradientText.style.background = "-webkit-linear-gradient(0deg, #f3904f, #574931, #f3904f, #574931)";
+    gradientText.style.background =
+      "-webkit-linear-gradient(0deg, #f3904f, #574931, #f3904f, #574931)";
   }
   gradientText.style.backgroundSize = "400% 400%";
   gradientText.style.webkitBackgroundClip = "text";
-}
+};
 
 const moveCursor = (e) => {
   mouseX = e.clientX;
   mouseY = e.clientY;
   requestAnimationFrame(updateCursor);
-}
+};
 
 const updateCursor = () => {
   cursor.style.top = `${mouseY - 10}px`;
   cursor.style.left = `${mouseX - 10}px`;
   descriptionWork.style.left = `${mouseX}px`;
   descriptionWork.style.top = `${mouseY}px`;
-}
+};
 
 const updateGridPos = () => {
-  scrollOffset = document.body.scrollHeight - window.innerHeight - window.scrollY;
+  scrollOffset =
+    document.body.scrollHeight - window.innerHeight - window.scrollY;
   grid.style.maskPosition = `calc(${mouseX}px - 28%) calc(${mouseY}px - 16vw - ${scrollOffset}px)`;
-}
+};
 
 const biggerCursor = document.querySelectorAll(".bigger-cursor");
 
@@ -71,7 +76,6 @@ document.addEventListener("mouseenter", () => {
   cursor.style.opacity = "1";
 });
 
-
 const checkbox = document.getElementById("dark-light-mode");
 const images = document.querySelectorAll(".image");
 
@@ -85,8 +89,9 @@ checkbox.addEventListener("change", () => {
   gradientText.style.filter = invertFilter ? "" : "invert(1)";
   document.querySelectorAll(".yellow-text").forEach((element) => {
     element.style.filter = invertFilter ? "" : "invert(1)";
-    element.style.color =  invertFilter ? "#ffc400" : "#000";
+    element.style.color = invertFilter ? "#ffc400" : "#000";
   });
+  descriptionWork.style.boxShadow = invertFilter ? "rgba(0, 0, 0, 0.36) 0px 22px 70px 4px" : "rgba(255, 255, 255, 0.36) 0px 22px 70px 4px";
 });
 
 /* https://webdesign.tutsplus.com/animate-on-scroll-with-javascript--cms-36671t */
@@ -134,32 +139,36 @@ window.addEventListener("scroll", () => {
   lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
 });
 
-const descriptionWork = document.querySelector('.description-work');
-const workClass = document.querySelectorAll('.work');
+const descriptionWork = document.querySelector(".description-work");
+const workClass = document.querySelectorAll(".work");
 let currentDescription = ""; // store current desc
 
 const updateDescription = (e) => {
   let target = e.target;
   // find ID that starts with
-  while (target && !target.id.startsWith('work-hover-')) {
+  while (target && !target.id.startsWith("work-hover-")) {
     target = target.parentElement;
   }
 
-  if (target && target.id.startsWith('work-hover-')) {
+  if (target && target.id.startsWith("work-hover-")) {
     let newDescription = ""; // store new desc
     switch (target.id) {
-      case 'work-hover-1':
-        newDescription = 'Paintings by Tim Lee<br><span class="yellow-text">Bootstrap, Custom CSS</span>';
+      case "work-hover-1":
+        newDescription =
+          'Paintings by Tim Lee<br><span class="yellow-text">Bootstrap, Custom CSS</span>';
         break;
-      case 'work-hover-2':
-        newDescription = 'Cookie clicker<br><span class="yellow-text">Custom CSS & JS</span>';
+      case "work-hover-2":
+        newDescription =
+          'Cookie clicker<br><span class="yellow-text">Custom CSS & JS</span>';
         break;
-        case 'work-hover-3':
-          newDescription = 'Modern school website<br><span class="yellow-text" style="">Bootstrap, Custom CSS</span>';
-          break;
-          case 'work-hover-4':
-            newDescription = 'Modern website for Pilsen Region<br><span class="yellow-text" style="">React, MUI, shadcn/ui, Tailwind CSS</span>';
-            break;
+      case "work-hover-3":
+        newDescription =
+          'Modern school website<br><span class="yellow-text" style="">Bootstrap, Custom CSS</span>';
+        break;
+      case "work-hover-4":
+        newDescription =
+          'Modern website for Pilsen Region<br><span class="yellow-text" style="">React, MUI, shadcn/ui, Tailwind CSS</span>';
+        break;
       default:
         newDescription = "";
     }
@@ -167,15 +176,16 @@ const updateDescription = (e) => {
     // updating overload fix
     if (newDescription !== currentDescription) {
       descriptionWork.innerHTML = newDescription;
-      const invertFilter = document.documentElement.style.filter !== "invert(1)";
+      const invertFilter =
+        document.documentElement.style.filter !== "invert(1)";
       document.querySelectorAll(".yellow-text").forEach((element) => {
         element.style.filter = invertFilter ? "" : "invert(1)";
-        element.style.color =  invertFilter ? "#ffc400" : "#000";
+        element.style.color = invertFilter ? "#ffc400" : "#000";
       });
       currentDescription = newDescription; // update current desc
     }
   }
-}
+};
 
 document.addEventListener("mousemove", (e) => {
   moveCursor(e);
@@ -190,14 +200,14 @@ document.addEventListener("scroll", (e) => {
 const startFollowing = () => {
   descriptionWork.style.opacity = "1";
   descriptionWork.style.scale = "1";
-}
+};
 
-const stopFollowing = () =>  {
+const stopFollowing = () => {
   descriptionWork.style.opacity = "0";
   descriptionWork.style.scale = "0.8";
-}
+};
 
-workClass.forEach(workClasses => {
-  workClasses.addEventListener('mouseover', startFollowing);
-  workClasses.addEventListener('mouseout', stopFollowing);
+workClass.forEach((workClasses) => {
+  workClasses.addEventListener("mouseover", startFollowing);
+  workClasses.addEventListener("mouseout", stopFollowing);
 });
