03c78fceea90db17d1ccbfe77d63cce5687c7c38
Ondřej Filip <ondrejfilipstranky@gmail.com>
2025-10-27 00:06:18 +0100

    Footer changes

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,8 @@
 export default function Footer() {
   return (
-    <div className="select-none flex justify-center items-end w-full text-zinc-800 tracking-tight text-[20.3vw] min-h-screen relative right-[0.5vw]">
+    <div className="flex justify-center items-end min-h-screen">
+    <div className="flex justify-center items-end select-none w-full bg-gradient-to-b from-zinc-800 to-zinc-400 bg-clip-text text-transparent tracking-tight text-[20.3vw] relative right-[0.5vw]">
       On<span className="font-dirtyline leading-[1.08]">D</span>ře<span className="font-dirtyline leading-[1.08]">J</span> Fili<span className="font-dirtyline leading-[1.08]">P</span>
-    </div>
+    </div></div>
   );
 }
